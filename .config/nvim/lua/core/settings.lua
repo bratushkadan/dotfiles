fba7763995627e1afa8f1927c7b5d12e3368cf32
bratushkadan <bratushkadan@gmail.com>
2024-03-23 23:47:12 +0300

    feat:refactor settings & add some more

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -1,7 +1,22 @@
 -- https://habr.com/ru/articles/64224/
-vim.api.nvim_command('set tabstop=4')
-vim.api.nvim_command('set shiftwidth=4')
-vim.api.nvim_command('set smarttab')
-vim.api.nvim_command('set expandtab')
-vim.api.nvim_command('set smartindent')
+
+-- Search
+vim.opt.ignorecase = true
+-- vim.opt.smartcase = true -- case-sensitive search
+vim.opt.showmatch = true
+
+-- Tabs
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Additional settings
+vim.opt.splitright = true -- vertical splits are rendered to the right
+vim.opt.splitbelow = true -- horizontal splits are rendered below
+vim.opt.clipboard = 'unnamedplus'
+-- opt.fixeol = false -- disable (?)
+-- opt.completeopt = 'menuone,noselect' -- built-in autocomplete
+-- vim.cmd [[autocmd BufEnter * set fo-=c fo-=r fo-=o]] -- do not make inserted line commented
 

b009bb1ca6bf6f1f12333d1415ca2eb58b3c8423
bratushkadan <bratushkadan@gmail.com>
2024-03-23 23:58:04 +0300

    feat:change telescope file searching

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,14 @@ require('telescope').setup({
     defaults = {
         file_ignore_patterns = {
             "node_modules",
-            "__pycache__"
+            "__pycache__",
+            "package-lock.json",
+            ".git"
+        }
+    },
+    pickers = {
+        find_files = {
+            hidden = true
         }
     },
     extensions = {

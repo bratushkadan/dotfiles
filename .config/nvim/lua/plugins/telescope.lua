b180e09e41ad6217d50148e6a847fc0a4e01b0fa
bratushkadan <bratushkadan@gmail.com>
2024-03-24 23:45:11 +0300

    feat:enhance telescope.nvim search file ignore list

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,8 @@ require('telescope').setup({
             "node_modules",
             "__pycache__",
             "package-lock.json",
-            ".git"
+            ".git",
+            "projects/webview/specs",
         }
     },
     pickers = {
@@ -30,3 +31,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+

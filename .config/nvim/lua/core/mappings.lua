5c5c62118c2fe28411835f363ade38ba0b8a4d3e
bratushkadan <bratushkadan@gmail.com>
2024-03-25 00:18:31 +0300

    feat:add important nvim paste mapping

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -27,4 +27,5 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('x', '<leader>p', '"_dP')
 

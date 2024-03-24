13ff83d8487e07c98a085c6767c3c75064b060da
bratushkadan <bratushkadan@gmail.com>
2024-03-24 14:14:34 +0300

    feat:enhance nvim settings.lua

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -6,16 +6,20 @@ vim.opt.ignorecase = true
 vim.opt.showmatch = true
 
 -- Tabs
-vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
 vim.opt.smarttab = true
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+-- Scrolling
+vim.opt.scrolloff = 8
 
 -- Additional settings
 vim.opt.splitright = true -- vertical splits are rendered to the right
 vim.opt.splitbelow = true -- horizontal splits are rendered below
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus' -- SHARED clipboard with the system
 -- opt.fixeol = false -- disable (?)
 -- opt.completeopt = 'menuone,noselect' -- built-in autocomplete
 -- vim.cmd [[autocmd BufEnter * set fo-=c fo-=r fo-=o]] -- do not make inserted line commented

15a8b8e1c1dd628ef1c9cabfde6b5fe5dc94dfae
bratushkadan <bratushkadan@gmail.com>
2024-03-26 23:00:31 +0300

    feat:use more pleasant of a theme

--- .config/nvim/lua/core/colors.lua
+++ .config/nvim/lua/core/colors.lua
@@ -4,7 +4,7 @@ function SetColor(color)
     color = color or "onedark"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    -- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
     vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
     vim.api.nvim_set_hl(0, "ColorColumn", {bg = "none"})
     vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})

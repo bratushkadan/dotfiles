beb4ba4c487073842a0acc2cd412d15741e143ad
bratushkadan <bratushkadan@gmail.com>
2025-02-28 08:00:32 +0300

    feat(nvim):add theme background color

--- .config/nvim/lua/core/colors.lua
+++ .config/nvim/lua/core/colors.lua
@@ -5,7 +5,6 @@ function SetColor(color)
     vim.cmd.colorscheme(color)
 
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
